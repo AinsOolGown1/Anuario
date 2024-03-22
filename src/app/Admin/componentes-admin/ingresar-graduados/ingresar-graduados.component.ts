99c4136c29b5185c83ac6d49b3c8d79b3178af2a
AinsOolGown1 <>
2024-03-22 10:37:41 -0600

    Validacion de los campos de agregar graduado

--- src/app/Admin/componentes-admin/ingresar-graduados/ingresar-graduados.component.ts
+++ src/app/Admin/componentes-admin/ingresar-graduados/ingresar-graduados.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ingresar-graduados',
@@ -6,6 +7,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./ingresar-graduados.component.scss'],
 })
 export class IngresarGraduadosComponent {
+  ingre_graduadoForm: FormGroup;
 
-
+  constructor(private fb: FormBuilder) {
+    this.ingre_graduadoForm = this.fb.group({
+      carnet:['', Validators.required],
+      nombres:['', Validators.required],
+      apellidos:['', Validators.required],
+      carrera:['', Validators.required],
+      facultad:['', Validators.required],
+      campus:['', Validators.required],
+      year_graduado:['', Validators.required],
+      estado_graduado:['', Validators.required],
+      destacado_graduado:['', Validators.required],
+      qr_graduado:['', Validators.required]
+    })
+  }
+  agregar_graduado(){
+    console.log(this.ingre_graduadoForm)
+  }
 }
