01764f312d8c9ca1b0c4663d44ff3c961a1bba38
AinsOolGown1 <>
2024-04-08 16:04:13 -0600

    Vista etiquetas anuario, falta agregar la foto y el css

--- src/app/Componentes/anuariovista/anuariovista.component.ts
+++ src/app/Componentes/anuariovista/anuariovista.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { GraduadosService } from 'src/app/Servicios/graduados.service';
+import { IngresarGraduados } from 'src/app/model/ingresar-graduados';
 
 @Component({
   selector: 'app-anuariovista',
@@ -6,5 +8,23 @@ import { Component } from '@angular/core';
   styleUrls: ['./anuariovista.component.scss']
 })
 export class AnuariovistaComponent {
+
+  listGraduados: IngresarGraduados[] = [];
+
+  constructor(private _graduadoService: GraduadosService) {}
+
+
+  ngOnInit(): void {
+    this.obtenerGraduados();
+  }
+
+  obtenerGraduados(): void {
+    this._graduadoService.getGraduados().subscribe((data: IngresarGraduados[]) => {
+      console.log(data);
+      this.listGraduados = data;
+    }, (error: any) => {
+      console.log(error);
+    });
+  }
 
 }
